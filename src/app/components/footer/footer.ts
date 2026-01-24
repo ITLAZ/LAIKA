5ac5218e19607bae84ce61b61c9f053c80ab049a
ITLAZ <juan.tapia.l@ucb.edu.bo>
2026-01-23 20:53:55 -0400

    footer y nav ya completados

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -17,4 +17,19 @@ export class Footer {
     { label: 'Donations', path: '/donations' },
     { label: 'Volunteer', path: '/volunteer' },
   ];
+
+  socialLinks = [
+    { icon: 'fa-brands fa-whatsapp', url: 'https://api.whatsapp.com/send?phone=59176708785', label: 'WhatsApp' },
+    { icon: 'fa-brands fa-facebook-f', url: 'https://www.facebook.com/CentroDeRescateAnimalLaika', label: 'Facebook' },
+    { icon: 'fa-brands fa-instagram', url: 'https://www.instagram.com/centroderescateanimallaika', label: 'Instagram' },
+    { icon: 'fa-brands fa-youtube', url: 'https://www.youtube.com/@CentroDeRescateAnimalLaika', label: 'YouTube' },
+    { icon: 'fa-brands fa-tiktok', url: 'https://www.tiktok.com/@rescateanimallaika', label: 'TikTok' },
+  ];
+
+  contactInfo = {
+    address: 'Av. Circunvalacion esquina Calle 8 de Peña Azul, La Paz - Bolivia',
+    email: 'centroderescateanimallaika@gmail.com',
+    phone: '+591 76708785 - +591 77741020',
+    hours: 'Lunes a Domingo: 10am - 6pm'
+  };
 }
